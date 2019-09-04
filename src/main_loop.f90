abfe0a92b6c70295e9258527e2d8e33836b1bf4e
Shiqi He <she010@uottawa.ca>
2019-09-04 09:52:27 -0400

    add main_loop.f90

---
+++ src/main_loop.f90
@@ -0,0 +1,46 @@
+
+
+PROGRAM MAIN_LOOP
+!-----------------------------------------------------------------------
+!   POISSON PROBLEM
+!-----------------------------------------------------------------------
+    USE MPI
+    USE BASIS
+    USE PARAM, ONLY: IERROR, MESHFILE
+    USE SET_MPI
+!    USE MESH
+!    USE GRAPH_PARTITION
+!    USE FIELDS
+    USE ADVECTION_DIFFUSION
+
+    IMPLICIT NONE
+    
+!    INTEGER :: I
+!    PRINT *, "CHECK"
+    
+    ! START MPI---------------------------------------------------------
+    CALL START_MPI
+    !-------------------------------------------------------------------
+    
+!    CALL READ_MESH(MESHFILE)
+    
+!    CALL BFS_NEW
+    
+!    CALL WRITE_TO_FILES
+    !-------------------------------------------------------------------
+    
+!    PRINT *, "CHECK"
+    
+    CALL DG_2D_WAVE
+    
+!    PRINT *, "CHECK"
+
+    
+    !-------------------------------------------------------------------
+    
+    ! END MPI-----------------------------------------------------------
+    CALL MPI_FINALIZE(IERROR)
+    !-------------------------------------------------------------------
+
+
+END PROGRAM MAIN_LOOP
