0541ef98b0f7c32495c5d8d9027cbbcd20ef22fa
Shiqi He <she010@uottawa.ca>
2019-09-06 09:46:41 -0400

    Completes the time derivative in y direction

--- src/dg_time_derivative.f90
+++ src/dg_time_derivative.f90
@@ -11,6 +11,8 @@ USE RIEMANN_SOLVER
 USE NODAL_2D_STORAGE
 USE RIEMANN_SOLVER
 USE FLUX_VECTORS
+USE EXTERNAL_STATE
+USE SPACTIAL_DERIVATIVE
 
 IMPLICIT NONE
 
@@ -79,6 +81,73 @@ SUBROUTINE DG_TIME_DER
         ENDDO
     
     ENDDO
+    !-------------------------------------------------------------------
+    
+    !-------------------------------------------------------------------
+    DEALLOCATE(NFLUX_X_L, NFLUX_X_R)
+    DEALLOCATE(FLUX_X)
+    DEALLOCATE(FLUX_DER_X)
+    DEALLOCATE(M_FIRST_DER_X)
+    !-------------------------------------------------------------------
+    
+    !-------------------------------------------------------------------
+    ALLOCATE(NFLUX_Y_D(NUM_OF_EQUATION), NFLUX_Y_U(NUM_OF_EQUATION))
+    ALLOCATE(FLUX_Y(0:M, NUM_OF_EQUATION))
+    ALLOCATE(FLUX_DER_Y(0:M, NUM_OF_EQUATION))
+    !-------------------------------------------------------------------
+    
+    ! Y DIRECTION-------------------------------------------------------
+    DO I=0, N
+        
+        X = GL_POINT_X(I)
+        
+        DO S=1, NUM_OF_EQUATION
+            CALL INTERPOLATE_TO_BOUNDARY(M, SOLUTION(I, :, S), &
+                                        LAGRANGE_DOWN, SOLUTION_INT_L(S))
+                                        
+                                        
+            CALL INTERPOLATE_TO_BOUNDARY(M, SOLUTION(I, :, S), &
+                                        LAGRANGE_UP, SOLUTION_INT_R(S))
+        
+        ENDDO
+        
+        CALL GET_EXTERNAL_STATE(NUM_OF_EQUATION, K_X, K_Y, &
+                                SOLUTION_INT_L, SOLUTION_EXT_L)
+        
+        CALL GET_EXTERNAL_STATE(NUM_OF_EQUATION, K_X, K_Y, &
+                                SOLUTION_INT_R, SOLUTION_EXT_R)
+                                
+        CALL RIEMANN(SOLUTION_INT_L, SOLUTION_EXT_L, NFLUX_Y_D, K_X, K_Y)
+        CALL RIEMANN(SOLUTION_INT_R, SOLUTION_EXT_R, NFLUX_Y_U, K_X, K_Y)
+        
+        DO J=0, M
+            CALL  YFLUX(SOLUTION(I, J, :), FLUX_Y(J, :))
+        
+        ENDDO
+    
+        CALL DG_SPACTIAL_DERIVATIVE(M, NFLUX_Y_D, NFLUX_Y_U, &
+                                    FLUX_Y, FLUX_DER_Y, M_FIRST_DER_Y, &
+                                    LAGRANGE_DOWN, LAGRANGE_UP, &
+                                    GL_W_Y)
+                                    
+        DO J=0, M
+            DO S=1, NUM_OF_EQUATION
+                SOLUTION_TIME_DER(I, J, S) = SOLUTION_TIME_DER(I, J, S) &
+                                            - FLUX_DER_Y(J, S)
+            ENDDO
+        ENDDO
+        
+    ENDDO
+    !-------------------------------------------------------------------
+    
+    !-------------------------------------------------------------------
+    DEALLOCATE(NFLUX_Y_D, NFLUX_Y_U)
+    DEALLOCATE(FLUX_Y)
+    DEALLOCATE(FLUX_DER_Y)
+    DEALLOCATE(M_FIRST_DER_Y)
+    DEALLOCATE(SOLUTION_INT_L, SOLUTION_INT_R)
+    DEALLOCATE(SOLUTION_EXT_L, SOLUTION_EXT_R)
+    !-------------------------------------------------------------------
 
 END SUBROUTINE DG_TIME_DER
 
