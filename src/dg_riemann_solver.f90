55f7d8e79bce243ef1781a2e6b013f7c2b0812b1
Shiqi He <she010@uottawa.ca>
2019-09-19 21:37:54 -0400

    Fixed Riemann solver

--- src/dg_riemann_solver.f90
+++ src/dg_riemann_solver.f90
@@ -12,15 +12,13 @@ IMPLICIT NONE
 
 CONTAINS
 
-SUBROUTINE RIEMANN(Q_L, Q_R, N_FLUX, ALPHA, BETA, NORMAL)
+SUBROUTINE RIEMANN(Q_L, Q_R, N_FLUX, UNIT_VECTOR, OUTWARD)
 !-----------------------------------------------------------------------
 ! ALGORITHM 88
 ! THE DERIVATION OF THE NUMERICAL FLUX IS KNOWN AS RIEMANN PROBLEM
 !-----------------------------------------------------------------------
 
     IMPLICIT NONE
-    
-    DOUBLE PRECISION, INTENT(IN) :: ALPHA, BETA !< TWO CONSTANT RELATED WITH WAVEVECTOR 
 
     DOUBLE PRECISION :: Q_L(NUM_OF_EQUATION)    !< SOLUTION AT LEFT
     DOUBLE PRECISION :: Q_R(NUM_OF_EQUATION)    !< SOLUTION AT RIGHT
@@ -32,22 +30,26 @@ SUBROUTINE RIEMANN(Q_L, Q_R, N_FLUX, ALPHA, BETA, NORMAL)
     
     DOUBLE PRECISION :: N_FLUX(NUM_OF_EQUATION) !< NUMERICAL FLUX
     
-    DOUBLE PRECISION :: NORMAL(2)
+    DOUBLE PRECISION :: UNIT_VECTOR(2)  !< COORDINATE AXIS UNIT VECTOR, E.G: X-AXIS (1.0D0, 0)
+    DOUBLE PRECISION :: OUTWARD !< BOUNDARY OUTWARD DIRECTION, ON THE LEFT = 1.0D0, ON THE RIGTH = 1.0D0
     
     P_L = Q_L(1); U_L = Q_L(2); V_L = Q_L(3);
     P_R = Q_R(1); U_R = Q_R(2); V_R = Q_R(3);
     
-    W_L =  P_L + C * ( ALPHA * U_L + BETA * V_L)
-    W_R =  P_R - C * ( ALPHA * U_R + BETA * V_R)
+    W_L =  P_L + C * ( UNIT_VECTOR(1) * U_L + UNIT_VECTOR(2) * V_L)
+    W_R =  P_R - C * ( UNIT_VECTOR(1) * U_R + UNIT_VECTOR(2) * V_R)
     
-    N_FLUX(1) = C * (W_L - W_R) / 2
-    N_FLUX(2) = NORMAL(1) * ALPHA * (W_L + W_R) / 2
-    N_FLUX(3) = NORMAL(2) * BETA * (W_L + W_R) / 2
+    N_FLUX(1) = C * (W_L - W_R) / 2 * OUTWARD
+    N_FLUX(2) = UNIT_VECTOR(1) * (W_L + W_R) / 2 * OUTWARD
+    N_FLUX(3) = UNIT_VECTOR(2) * (W_L + W_R) / 2 * OUTWARD
 
 END SUBROUTINE RIEMANN
 
 SUBROUTINE RIEMANN_X(Q_L, Q_R, N_FLUX, NORMAL)
-
+!-----------------------------------------------------------------------
+! Riemann solver in x direction
+! Use upwind fluxes
+!-----------------------------------------------------------------------
     IMPLICIT NONE
     
     DOUBLE PRECISION :: Q_L(NUM_OF_EQUATION)    !< SOLUTION AT LEFT
@@ -76,6 +78,10 @@ END SUBROUTINE RIEMANN_X
 
 
 SUBROUTINE RIEMANN_Y(Q_L, Q_R, N_FLUX, NORMAL)
+!-----------------------------------------------------------------------
+! Riemann solver in y direction
+! Use upwind fluxes
+!-----------------------------------------------------------------------
 
     IMPLICIT NONE
     
