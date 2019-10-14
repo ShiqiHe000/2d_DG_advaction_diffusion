5fe4505f11215ec5a8269cff9ac0760d73cc6c59
Shiqi He <she010@uottawa.ca>
2019-10-14 19:29:18 -0400

    nothing

--- src/dg_param.f90
+++ src/dg_param.f90
@@ -11,12 +11,12 @@ MODULE PARAM
     ! variables you must change =======================================
     
     !< INITIAL MESH FILE------------------------------------------------
-    CHARACTER(LEN=*), PARAMETER :: MESHFILE = "1_element.msh"    
+    CHARACTER(LEN=*), PARAMETER :: MESHFILE = "4_elements.msh"    
     !-------------------------------------------------------------------
     
     ! SET POLYNOMIAL ORDER----------------------------------------------
-    INTEGER :: N = 10   !< POLYNOMIAL DEGREE IN X DIRECTION
-    INTEGER :: M = 10    !< POLYNOMIAL DEGREE IN Y DIRECTION
+    INTEGER :: N = 4   !< POLYNOMIAL DEGREE IN X DIRECTION
+    INTEGER :: M = 4    !< POLYNOMIAL DEGREE IN Y DIRECTION
     INTEGER :: NMAX = 10   !< MAXIMUM POLYNOMIAL DEGREE IN X DIRECTION
     INTEGER :: MMAX = 10   !< MAXIMUM POLYNOMIAL DEGREE IN Y DIRECTION
     !-------------------------------------------------------------------
@@ -30,16 +30,16 @@ MODULE PARAM
     !-------------------------------------------------------------------
     
     ! NUMBER OF ELEMENT-------------------------------------------------
-    INTEGER :: EXP_X = 0       !< WRITE ELEMENT NUMBER IN X DIRECTION EXPONENTIAL ORDER, I.E., 2^(EXP_X)
-    INTEGER :: EXP_Y = 0       !< WRITE ELEMENT NUMBER IN Y DIRECTION EXPONENTIAL ORDER, I.E., 2^(EXP_Y)
+    INTEGER :: EXP_X = 1       !< WRITE ELEMENT NUMBER IN X DIRECTION EXPONENTIAL ORDER, I.E., 2^(EXP_X)
+    INTEGER :: EXP_Y = 1       !< WRITE ELEMENT NUMBER IN Y DIRECTION EXPONENTIAL ORDER, I.E., 2^(EXP_Y)
     !-------------------------------------------------------------------
     
     ! TIME--------------------------------------------------------------
 !    DOUBLE PRECISION :: T_TOTAL = 0.0d0     !< TOTAL TIME INTEGRAL
 !    DOUBLE PRECISION :: T_TOTAL = (2.0e-4)*2.0d0     !< TOTAL TIME INTEGRAL
 !    INTEGER :: NT = 2                    !< TIME STEP NUMBER
-    DOUBLE PRECISION :: T_TOTAL = 2.0D0     !< TOTAL TIME INTEGRAL
-    INTEGER :: NT = 10000                   !< TIME STEP NUMBER
+    DOUBLE PRECISION :: T_TOTAL = 1.0D0     !< TOTAL TIME INTEGRAL
+    INTEGER :: NT = 100000                   !< TIME STEP NUMBER
     !-------------------------------------------------------------------
     
     ! variables you could change =======================================
