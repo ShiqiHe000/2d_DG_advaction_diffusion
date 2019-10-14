0b0ec6f474e2e5205d292d35ccc27f58b033b07b
Shiqi He <she010@uottawa.ca>
2019-10-14 16:38:50 -0400

    something is wrong

--- src/dg_param.f90
+++ src/dg_param.f90
@@ -38,7 +38,7 @@ MODULE PARAM
 !    DOUBLE PRECISION :: T_TOTAL = 0.0d0     !< TOTAL TIME INTEGRAL
 !    DOUBLE PRECISION :: T_TOTAL = (2.0e-4)*2.0d0     !< TOTAL TIME INTEGRAL
 !    INTEGER :: NT = 2                    !< TIME STEP NUMBER
-    DOUBLE PRECISION :: T_TOTAL = 1.0D0     !< TOTAL TIME INTEGRAL
+    DOUBLE PRECISION :: T_TOTAL = 2.0D0     !< TOTAL TIME INTEGRAL
     INTEGER :: NT = 10000                   !< TIME STEP NUMBER
     !-------------------------------------------------------------------
     
