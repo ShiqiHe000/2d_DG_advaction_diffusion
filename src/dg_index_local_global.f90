54b7ce1261c4a25c885ed239e61208a761a0e19c
Shiqi He <she010@uottawa.ca>
2019-11-05 21:33:46 -0500

    create subroutine to transform between local index and global index

--- src/dg_index_local_global.f90
+++ src/dg_index_local_global.f90
@@ -28,6 +28,20 @@ SUBROUTINE INDEX_GLOBAL_TO_LOCAL(RANK, G_INDEX, L_INDEX)
 END SUBROUTINE INDEX_GLOBAL_TO_LOCAL
 
 
+!-----------------------------------------------------------------------
+!> Input rank and element local index, output global index.
+!-----------------------------------------------------------------------
+SUBROUTINE INDEX_LOCAL_TO_GLOBAL(RANK, L_INDEX, G_INDEX)
+
+    IMPLICIT NONE
+    
+    INTEGER, INTENT(IN) :: RANK !< LOCAL RANK NUMBER
+    INTEGER, INTENT(OUT) :: G_INDEX  !< ELEMENT GLOBAL INDEX
+    INTEGER, INTENT(IN) :: L_INDEX  !< ELEMENT LOCAL INDEX
+    
+    G_INDEX = L_INDEX + ELEM_RANGE(RANK) + 1
+
+END SUBROUTINE INDEX_LOCAL_TO_GLOBAL
 
 END MODULE INDEX_LOCAL_GLOBAL
 
